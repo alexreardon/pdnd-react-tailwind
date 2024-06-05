2767e1341b994d6d9bc405225bc4648754de8b93
Alex Reardon <areardon@atlassian.com>
2024-06-06 09:31:36 +1000

    icon done

--- src/item.tsx
+++ src/item.tsx
@@ -1,9 +1,13 @@
 import { TItem } from './types';
+import { GripVertical } from 'lucide-react';
 
 export function Item({ item }: { item: TItem }) {
   return (
-    <div className="border border-solid rounded p-2 hover:bg-slate-100 hover:cursor-grab">
-      Item: {item.id}
+    <div className="flex flex-row items-center border border-solid rounded p-2 pl-0 hover:bg-slate-100 hover:cursor-grab">
+      <div className="w-6 flex justify-center">
+        <GripVertical size={10} />
+      </div>
+      <span>Item: ({item.id})</span>
     </div>
   );
 }
